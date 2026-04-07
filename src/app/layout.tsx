904e210c404461ddf48891de441775dd5cdb506d
Aleksa Simovic <asesa1312@gmail.com>
2026-04-08 00:02:40 +0200

    style: swap default font for inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,20 @@
 import "~/styles/globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import { cn } from "~/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Image Inverter",
   description: "Lightweight bulk image inverter."
 };
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans"
-});
-
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
-      className={cn(geist.variable, "font-sans", inter.variable)}
+      className={cn(inter.variable, "font-sans", inter.variable)}
       lang="en"
     >
       <body>{children}</body>
