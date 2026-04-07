472379cd4dfe109268a5cd6dfad36bb63284e48a
Aleksa Simovic <asesa1312@gmail.com>
2026-04-07 23:58:17 +0200

    feat: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import "~/styles/globals.css";
 
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Inter } from "next/font/google";
+import { cn } from "~/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Image Inverter",
@@ -16,7 +19,7 @@ const geist = Geist({
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
-      className={`${geist.variable}`}
+      className={cn(geist.variable, "font-sans", inter.variable)}
       lang="en"
     >
       <body>{children}</body>
