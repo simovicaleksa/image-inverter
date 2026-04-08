36b2fa79125dc483fd3d5f5189a447718ee91777
Aleksa Simovic <asesa1312@gmail.com>
2026-04-09 00:39:52 +0200

    style: improve progress ux

--- src/hooks/use-progress-dialog.tsx
+++ src/hooks/use-progress-dialog.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LucideFolderDown } from "lucide-react";
 import {
 	createContext,
 	type Dispatch,
@@ -17,8 +18,16 @@ import {
 	AlertDialogTitle,
 } from "~/components/ui/alert-dialog";
 import { Button } from "~/components/ui/button";
+import {
+	Item,
+	ItemContent,
+	ItemDescription,
+	ItemMedia,
+	ItemTitle,
+} from "~/components/ui/item";
 import { Progress } from "~/components/ui/progress";
 import { zipFiles } from "~/lib/file";
+import { formatBytes } from "~/lib/format";
 import { invertImage } from "~/lib/image";
 import { useFiles } from "./use-files";
 
@@ -43,7 +52,11 @@ export function ProgressDialogProvider({
 
 	const [progress, setProgress] = useState(0);
 	const [isOpen, setIsOpen] = useState(false);
-	const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+	const [zipUrl, setZipUrl] = useState<string | null>(null);
+	const [zipSize, setZipSize] = useState(0);
+
+	const imagesLeft = files.length - Math.floor((files.length * progress) / 100);
+	const isComplete = progress === 100;
 
 	const invertBegin = useCallback(async () => {
 		setIsOpen(true);
@@ -53,23 +66,33 @@ export function ProgressDialogProvider({
 		for (const file of files) {
 			const invertedImage = await invertImage(file.file);
 			results.push(invertedImage);
-			setProgress((files.length / results.length) * 100);
+			setProgress((results.length / files.length) * 100);
 		}
 
-		const url = await zipFiles(results);
-		setDownloadUrl(url);
+		const zip = await zipFiles(results);
+		setZipUrl(zip.url);
+		setZipSize(zip.blob.size);
 	}, [files]);
 
 	function handleDownloadClick() {
-		if (!downloadUrl) return;
+		if (!zipUrl) return;
 		const a = document.createElement("a");
-		a.href = downloadUrl;
+		a.href = zipUrl;
 		a.download = "images.zip";
 		a.click();
-		URL.revokeObjectURL(downloadUrl);
+		URL.revokeObjectURL(zipUrl);
 		setIsOpen(false);
-		setDownloadUrl(null);
+		setZipUrl(null);
+		setZipSize(0);
 		setProgress(0);
+	}
+
+	function handleCancelClick() {
+		setIsOpen(false);
+		setZipUrl(null);
+		setZipSize(0);
+		setProgress(0);
+		if (zipUrl) URL.revokeObjectURL(zipUrl);
 	}
 
 	return (
@@ -86,17 +109,27 @@ export function ProgressDialogProvider({
 				<AlertDialogContent>
 					<AlertDialogHeader>
 						<AlertDialogTitle className="flex flex-row items-center gap-2">
-							Processing {files.length} images
+							{isComplete ? "Inverting completed" : "Inverting images"}
 						</AlertDialogTitle>
-						<AlertDialogDescription></AlertDialogDescription>
+						<AlertDialogDescription>
+							{isComplete
+								? "Download your images."
+								: `${imagesLeft} ${imagesLeft === 1 ? "image" : "images"} left`}
+						</AlertDialogDescription>
 					</AlertDialogHeader>
-					<Progress value={progress}></Progress>
+					<div className="flex size-full flex-col items-center justify-center">
+						{isComplete ? (
+							<ItemComplete name="images.zip" size={formatBytes(zipSize)} />
+						) : (
+							<Progress className="w-full" value={progress}></Progress>
+						)}
+					</div>
+
 					<AlertDialogFooter>
-						<Button
-							className="w-full"
-							disabled={!downloadUrl}
-							onClick={handleDownloadClick}
-						>
+						<Button onClick={handleCancelClick} variant={"outline"}>
+							Cancel
+						</Button>
+						<Button disabled={!zipUrl} onClick={handleDownloadClick}>
 							Download
 						</Button>
 					</AlertDialogFooter>
@@ -104,6 +137,20 @@ export function ProgressDialogProvider({
 			</AlertDialog>
 			{children}
 		</ProgressDialogContext.Provider>
+	);
+}
+
+function ItemComplete({ name, size }: { name: string; size: string }) {
+	return (
+		<Item variant={"outline"}>
+			<ItemMedia>
+				<LucideFolderDown className="size-6" />
+			</ItemMedia>
+			<ItemContent>
+				<ItemTitle>{name}</ItemTitle>
+				<ItemDescription>{size}</ItemDescription>
+			</ItemContent>
+		</Item>
 	);
 }
 
