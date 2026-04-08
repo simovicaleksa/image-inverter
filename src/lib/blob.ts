e05f45cf2fb18160a67c52c493b2bb723c75ebb3
Aleksa Simovic <asesa1312@gmail.com>
2026-04-08 23:45:08 +0200

    feat: add canvas to blob util

---
+++ src/lib/blob.ts
@@ -0,0 +1,8 @@
+export function canvasToBlob(canvas: HTMLCanvasElement): Promise<Blob> {
+	return new Promise((resolve, reject) => {
+		canvas.toBlob((blob) => {
+			if (blob) resolve(blob);
+			else reject(new Error("Failed to convert canvas to blob"));
+		});
+	});
+}
